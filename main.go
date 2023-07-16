a7e63c6ee2d83ae8ee70214a6752af174fa600f2
Pranav Shikarpur <pranav.shikarpur@pangea.cloud>
2023-07-16 13:18:48 -0400

    fix: fixed package typo

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/pangeacyber/cli/cmd"
+import "github.com/pangeacyber/pangea-cli/cmd"
 
 func main() {
 	cmd.Execute()
