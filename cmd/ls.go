bd54c70817f56a471f62974003daebc672a9ce49
Diego Bendersky <diego.bendersky@pangea.cloud>
2024-04-09 11:27:54 -0300

    --show-secrets flag for ls command

--- cmd/ls.go
+++ cmd/ls.go
@@ -5,6 +5,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -17,9 +19,17 @@ var lsCmd = &cobra.Command{
 	
 	shows you all the secrets in your current selected workspace.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		show, err := cmd.Flags().GetBool("show-secrets")
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		remoteEnv := Get_env()
 
 		for _, envVar := range remoteEnv {
+			if !show {
+				envVar = strings.Split(envVar, "=")[0] + "=********"
+			}
 			fmt.Println(envVar)
 		}
 	},
@@ -27,4 +37,7 @@ var lsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(lsCmd)
+
+	lsCmd.Flags().BoolP("show-secrets", "s", false, "Show the secret values")
+
 }
