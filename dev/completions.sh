46162dc592786eae532b47952f3c7806dc7be7f9
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-10-16 09:45:46 -0300

    Fix `main.go` path

--- dev/completions.sh
+++ dev/completions.sh
@@ -5,5 +5,5 @@ rm -rf completions
 mkdir completions
 
 for sh in bash zsh fish; do
-	go run main.go completion "$sh" > "completions/pangea.$sh"
+	go run cmd/main.go completion "$sh" > "completions/pangea.$sh"
 done
